d8b23686d5bed2ea77ec3b3ff4f7e9f21763fa05
santhoshshyam107@gmail.com <santhoshshyam107@gmail.com>
2026-01-05 17:38:30 +0530

    Cart

---
+++ E2E/AddToCart.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('Add To Cart Functionality', async({page})=>{
+    await page.goto('https://www.saucedemo.com/');
+
+    // Login
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+})
